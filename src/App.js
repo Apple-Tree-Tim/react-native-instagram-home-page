2a9ef661704555a7ea0c7d856191b2804635fdf2
Apple-Tree-Tim <timbeamon11910@gmail.com>
2022-08-15 18:47:46 +0300

    Project was sketched

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, StyleSheet} from 'react-native';
 
 function App() {
   return (
-    <SafeAreaView>
-      <Text>Welcome my clone instagram home page!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+      </View>
+      <View style={styles.stories}>
+      </View>
+      <View style={styles.posts}>
+      </View>
+      <View style={styles.footer}>
+      </View>
     </SafeAreaView>
   )
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+
+  stories: {
+    flex: 3,
+    backgroundColor: 'blue',
+  },
+
+  posts: {
+    flex: 7,
+    backgroundColor: 'green',
+  },
+
+  footer: {
+    flex: 1,
+    backgroundColor: 'purple',
+  },
+
+})
